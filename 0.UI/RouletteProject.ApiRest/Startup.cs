420eba02ed1cbc3a73a04510793defca6d0881f8
Juan Camilo González <jcgonzalezalzate@gmail.com>
2021-04-30 21:50:53 -0500

    MASIV-001: Some style fixing when resolve dependencies.

--- 0.UI/RouletteProject.ApiRest/Startup.cs
+++ 0.UI/RouletteProject.ApiRest/Startup.cs
@@ -1,18 +1,13 @@
-using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DataModel;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using RouletteProject.ApiRest.Models;
-using RouletteProject.Infrastructure.IoC;
-using System;
-using System.Linq;
-using REDIS = StackExchange.Redis;
-
 namespace RouletteProject.ApiRest
 {
+    using Infrastructure.Helpers;
+    using Infrastructure.IoC;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,45 +19,13 @@ namespace RouletteProject.ApiRest
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var dynamoDbConfig = Configuration.GetSection("DynamoDb");
-            var runLocalDynamoDb = dynamoDbConfig.GetValue<bool>("LocalMode");
-
             services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
-            
-            if (runLocalDynamoDb)
-            {
-                services.AddSingleton<IAmazonDynamoDB>(sp =>
-                {
-                    var clientConfig = new AmazonDynamoDBConfig { ServiceURL = dynamoDbConfig.GetValue<string>("LocalServiceUrl") };
-                    return new AmazonDynamoDBClient(clientConfig);
-                });
-            }
-            else
-            {
-                services.AddAWSService<IAmazonDynamoDB>();
-            }
-
-            services.AddTransient<IDynamoDBContext, DynamoDBContext>();
-
-            services.AddSingleton(provider => new RedisConfiguration
-            {
-                ConnectionStringTxn = Configuration.GetValue<string>("redisCnstring")
-            });
-            services.AddSingleton<REDIS.IServer>(CreateRedisServerForManagement);
-
-            services.AddStackExchangeRedisCache(opt =>
-            {
-                var redisConfig = services.BuildServiceProvider().GetService<RedisConfiguration>();
-                opt.Configuration = redisConfig.ConnectionStringTxn;
-            });
-
+            Logger.Current.Debug("Startup");
             new DependencyResolver().SetServiceCollection(services);
-
             services.AddControllers();
-
             services.AddSwaggerGen(c => { c.EnableAnnotations(); });
         }
-
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwagger();
@@ -85,21 +48,6 @@ namespace RouletteProject.ApiRest
             {
                 endpoints.MapControllers();
             });
-        }
-
-        private REDIS.IServer CreateRedisServerForManagement(IServiceProvider provider)
-        {
-            var redisConfig = provider.GetService<RedisConfiguration>();
-            var cnstringAdmin = redisConfig.ConnectionStringAdmin;
-            //You need allowAdmin=true to call methods .FlushDatabase and .Keys()
-            //https://stackexchange.github.io/StackExchange.Redis/Basics.html
-            var redis = REDIS.ConnectionMultiplexer.Connect(cnstringAdmin);
-            var firstEndPoint = redis.GetEndPoints().FirstOrDefault();
-            if (firstEndPoint == null)
-            {
-                throw new ArgumentException("The endpoints collection was empty. Could not get an end point from Redis connection multiplexer.");
-            }
-            return redis.GetServer(firstEndPoint);
         }
     }
 }
--- 3.Infrastructure/RouletteProject.Infrastructure.IoC/DependencyResolver.cs
+++ 3.Infrastructure/RouletteProject.Infrastructure.IoC/DependencyResolver.cs
@@ -1,25 +1,41 @@
-﻿using RouletteProject.Domain.Services.Bet;
-
-namespace RouletteProject.Infrastructure.IoC
+﻿namespace RouletteProject.Infrastructure.IoC
 {
+    using Amazon.DynamoDBv2;
+    using Amazon.DynamoDBv2.DataModel;
     using Application.Interfaces;
     using Application.Interfaces.Generics;
     using Application.Services;
     using Application.Services.Generics;
     using Domain.Entities;
+    using Domain.Entities.Configuration;
     using Domain.Interfaces.Repositories;
     using Domain.Interfaces.Services;
     using Domain.Services;
+    using Domain.Services.Bet;
     using Domain.Services.Generics;
     using Microsoft.Extensions.DependencyInjection;
     using Repo;
     using Repo.Generics;
+    using System;
+    using System.Linq;
+    using REDIS = StackExchange.Redis;
 
     public class DependencyResolver
     {
         public IServiceCollection SetServiceCollection(IServiceCollection services)
         {
+            ResolveDynamoDb(services);
+            ResolveRedis(services);
+
             services.AddTransient<RepositoryContext>();
+            services.AddTransient(s => new BetConfiguration
+            {
+                MaximumRoulletteNumber = Convert.ToInt32(Environment.GetEnvironmentVariable("MAXIMUM_ROULLETTE_NUMBER")),
+                MinimumRoulletteNumber = Convert.ToInt32(Environment.GetEnvironmentVariable("MINIMUM_ROULLETTE_NUMBER")),
+                MultiplyingFactorByColour = Convert.ToDecimal(Environment.GetEnvironmentVariable("MULTIPLYING_FACTOR_BY_COLOUR")),
+                MultiplyingFactorByNumber = Convert.ToDecimal(Environment.GetEnvironmentVariable("MULTIPLYING_FACTOR_BY_NUMBER"))
+            });
+
             services.AddTransient<IDynamoRepository, DynamoRepository>();
             services.AddTransient<ICacheRepository, CacheRepository>();
 
@@ -36,6 +52,51 @@ namespace RouletteProject.Infrastructure.IoC
             services.AddTransient<IGenericApplication<Bet>, GenericApplication<Bet>>();
 
             return services;
+        }
+
+        private void ResolveRedis(IServiceCollection services)
+        {
+            services.AddSingleton(provider => new RedisConfiguration
+            {
+                ConnectionStringTxn = Environment.GetEnvironmentVariable("REDIS_LOCALSTACK_HOSTNAME")
+            });
+
+            services.AddSingleton(CreateRedisServerForManagement);
+            services.AddStackExchangeRedisCache(opt =>
+            {
+                var redisConfig = services.BuildServiceProvider().GetService<RedisConfiguration>();
+                opt.Configuration = redisConfig.ConnectionStringTxn;
+            });
+        }
+
+        private void ResolveDynamoDb(IServiceCollection services)
+        {
+            services.AddSingleton<IAmazonDynamoDB>(sp =>
+            {
+                var clientConfig = new AmazonDynamoDBConfig
+                {
+                    ServiceURL = Environment.GetEnvironmentVariable("DYNAMO_LOCALSTACK_HOSTNAME")
+                };
+
+                return new AmazonDynamoDBClient(clientConfig);
+            });
+
+            services.AddTransient<IDynamoDBContext, DynamoDBContext>();
+        }
+
+        private REDIS.IServer CreateRedisServerForManagement(IServiceProvider provider)
+        {
+            var redisConfig = provider.GetService<RedisConfiguration>();
+            var cnstringAdmin = redisConfig.ConnectionStringAdmin;
+            var redis = REDIS.ConnectionMultiplexer.Connect(cnstringAdmin);
+            var firstEndPoint = redis.GetEndPoints().FirstOrDefault();
+
+            if (firstEndPoint == null)
+            {
+                throw new ArgumentException("The endpoints collection was empty. Could not get an end point from Redis connection multiplexer.");
+            }
+
+            return redis.GetServer(firstEndPoint);
         }
     }
 }
